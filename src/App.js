c20260c6ee500ea0df7aae24d8bce771e23fa0a1
jannet18 <janetngei18@gmail.com>
2023-11-08 15:06:50 +0300

    top buttons styled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Menu from './components/Menu.js';
 import Container from './components/Container.js';
-import MainContainer from './components/MainContainer.js';
 
 function App() {
   return (
     <div className="App">
       <Menu/>
       <Container/>
-      <MainContainer/>
     </div>
+ 
   );
 }
 
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,47 @@
 import React from 'react';
-// import './MenuContainer.css';
-import './'
+import '../components/MainContainer.css';
+import banner from '../images/backround.jpeg';
+
 
 function MainContainer() {
   return (
     <div className='mainContainer'>
-        <div className='left'></div>
+        <div className='left'>
+            <div className="banner"
+              style = {{
+                background : `url(${banner})`,
+                backgroundRepeat : 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover'
+              }}>
+                 <div className="text-container">
+              <h1>Round Hall</h1>
+              <h2>1.5 ETH</h2>
+              <p>Uploaded by  Alexander Udinoff</p>
+              <div className="bid">
+                <a href="#button" className='button-1'>Bid now</a>
+                <p>Ending In <span>2d:15h:20m</span></p>
+              </div>
+            </div>
+            </div> 
+        <div className="cards">
+          <div className="filters">
+            <div className="popular">
+              <h2>Feed</h2>
+              <a href='#button-2' className='button-2'>Popular</a>
+            </div>
+            <div className="filter-buttons">
+              <a href="#btn" className="button-1">All</a>
+              <a href="#btn" className="button-2">Illustration</a>
+              <a href="#btn" className="button-2">Art</a>
+              <a href="#btn" className="button-2">Games</a>
+            </div>
+          </div>
+        </div>
+        <main>
+          
+        </main>
+        </div>
         <div className='right '></div>
     </div>
   )
