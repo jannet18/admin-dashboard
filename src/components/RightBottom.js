f12f93bd556d5edd3e8b2e87ce3ea29920b16638
jannet18 <janetngei18@gmail.com>
2023-11-10 23:54:04 +0300

    complete styling

--- src/components/RightBottom.js
+++ src/components/RightBottom.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import TopSeller from './TopSeller';
 
 function RightBottom() {
   return (
@@ -6,8 +7,23 @@ function RightBottom() {
         <div className="bottom_name">
         <h2>Top Seller</h2>
         <a href="#view-more" className="view_more">View More</a>
+        </div>
+        {
+            TopSeller && TopSeller.map((seller, id) => (
+                // console.log(seller)
+            <div className="top-seller" key={id}>
+                <div className="top-seller_img">
+                    <img src={seller?.imgSrc} alt="" />
+                </div>
+           
+            <div className="top-seller_name">
+                            <p>{seller?.seller_name}<span>{seller?.username}</span></p>
             </div>
-     
+            <a href="#btn" className="button-1 btn">Follow</a>
+            </div>
+            ))
+        }
+       
     </div>
   )
 }
---
+++ src/components/TopSeller.js
@@ -0,0 +1,17 @@
+import seller1 from '../images/seller-1.jpeg';
+import seller2 from '../images/seller-2.jpeg';
+import seller3 from '../images/seller-3.jpeg';
+import seller4 from '../images/seller-4.jpeg';
+import seller5 from '../images/seller-5.jpeg';
+
+
+    const TopSeller = [
+        {id : 1, seller_name : 'London Cooper', username : '@london', imgSrc : seller1},
+        {id : 2, seller_name : 'Marley Sasha', username : '@marley', imgSrc : seller2}, 
+        {id : 3, seller_name : 'Malka Marcus', username : '@malka', imgSrc : seller3}, 
+        {id : 4, seller_name : 'Donald T', username : '@donald', imgSrc : seller4}, 
+        {id : 5, seller_name : 'Harold K', username : '@harold', imgSrc : seller5}, 
+
+    ]
+
+export default TopSeller;
